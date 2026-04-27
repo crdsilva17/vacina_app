987ab5ee7dd3dc853c2b5ea279a3f2ca9b70393f
Cristiano <cristianoatmcrs@gmail.com>
2026-04-27 15:14:02 -0300

    setting ip for test

--- lib/data/repositories/local_repository.dart
+++ lib/data/repositories/local_repository.dart
@@ -3,19 +3,23 @@ import 'dart:convert';
 import 'package:vacina_app/data/http/http_client.dart';
 import 'package:vacina_app/data/models/local_model.dart';
 
+import 'package:flutter/foundation.dart';
+
 abstract class ILocalRepository {
   Future<List<LocalModel>> getLocal();
 }
 
 class LocalRepository implements ILocalRepository {
   final IHttpClient client;
-
+  final String baseUrl = kIsWeb
+      ? 'http://localhost:8080/api/v1/locais'
+      : 'http://10.0.2.2:8080/api/v1/locais';
   LocalRepository({required this.client});
 
   @override
   Future<List<LocalModel>> getLocal() async {
     final response = await client.get(
-      url: 'http://localhost:8080/api/v1/locais',
+      url: baseUrl,
     );
 
     if (response.statusCode == 200) {
