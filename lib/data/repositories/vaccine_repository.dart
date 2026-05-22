16349349bd346ce9f1690b6e1c7dac76f63d0bb2
Cristiano <cristianoatmcrs@gmail.com>
2026-05-22 02:16:48 -0300

    Create method getVaccines()

--- lib/data/repositories/vaccine_repository.dart
+++ lib/data/repositories/vaccine_repository.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:vacina_app/data/dto/vaccine_request.dart';
+import 'package:vacina_app/data/http/api_endpoints.dart';
+import 'package:vacina_app/data/http/http_client.dart';
 import 'package:vacina_app/data/models/vaccine_model.dart';
 
 abstract class IVaccineRepository {
@@ -6,4 +11,49 @@ abstract class IVaccineRepository {
   Future<void> postVaccine(VaccineRequest vaccine);
   Future<void> deleteVaccine(String id);
   Future<void> putVaccine(String id);
+}
+
+class VaccineRepository implements IVaccineRepository {
+  final IHttpClient client;
+  final Map<String, String> url = {
+    'base': ApiEndpoints.baseUrl,
+    'endpoint': ApiEndpoints.vacinas,
+  };
+
+  VaccineRepository({required this.client});
+
+  @override
+  Future<void> deleteVaccine(String id) async {
+    // TODO: implement deleteVaccine
+    return;
+  }
+
+  @override
+  Future<List<VaccineModel>> getVaccines() async {
+    final SharedPreferences shared = await SharedPreferences.getInstance();
+    final String? token = shared.getString('token');
+    final response = await client.getAuth(uri: url, token: token.toString());
+    List<VaccineModel> vaccines = [];
+
+    if (response.statusCode == 200) {
+      final body = jsonDecode(response.body);
+      body.map((vaccine) {
+        final VaccineModel vaccineModel = VaccineModel.fromJson(vaccine);
+        vaccines.add(vaccineModel);
+      }).toList();
+    }
+    return vaccines;
+  }
+
+  @override
+  Future<void> postVaccine(VaccineRequest vaccine) async {
+    // TODO: implement postVaccine
+    return;
+  }
+
+  @override
+  Future<void> putVaccine(String id) async {
+    // TODO: implement putVaccine
+    return;
+  }
 }
