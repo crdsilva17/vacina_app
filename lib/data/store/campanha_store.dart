2cf018948d41b1f9b5fc8e4e8917be750bf3cd83
Cristiano <cristianoatmcrs@gmail.com>
2026-07-09 17:40:30 -0300

    Create Campanha Store

---
+++ lib/data/store/campanha_store.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/widgets.dart';
+import 'package:vacina_app/data/dto/campanha_request.dart';
+import 'package:vacina_app/data/models/campanha_model.dart';
+import 'package:vacina_app/data/repositories/campanha_repository.dart';
+
+class CampanhaStore {
+  final ICampanhaRepository repository;
+  final ValueNotifier<List<CampanhaModel>> stateList = ValueNotifier([]);
+  final ValueNotifier<bool> isLoading = ValueNotifier(false);
+  final ValueNotifier<String> error = ValueNotifier('');
+
+  CampanhaStore({required this.repository});
+
+  Future<void> getList() async {
+    isLoading.value = true;
+    try {
+      stateList.value = await repository.buscarTodos();
+    } catch (e) {
+      error.value = 'Error ao buscar Campanhas';
+    } finally {
+      isLoading.value = false;
+    }
+  }
+
+  Future<void> getByLocalId(String localId) async {
+    isLoading.value = true;
+    try {
+      stateList.value = await repository.buscarPorLocalId(localId);
+    } catch (e) {
+      error.value = 'Error ao buscar por UBS';
+    } finally {
+      isLoading.value = false;
+    }
+  }
+
+  Future<void> criar(CampanhaRequest request) async {
+    isLoading.value = true;
+    try {
+      await repository.criar(request);
+      stateList.value = await repository.buscarTodos();
+    } catch (e) {
+      error.value = 'Error ao criar nova Campanha';
+    } finally {
+      isLoading.value = false;
+    }
+  }
+
+  Future<void> atualizar(String id, CampanhaRequest request) async {
+    isLoading.value = true;
+    try {
+      await repository.atualizar(id, request);
+      stateList.value = await repository.buscarTodos();
+    } catch (e) {
+      error.value = 'Error ao atualizar Campanha';
+    } finally {
+      isLoading.value = false;
+    }
+  }
+
+  Future<void> deletar(String id) async {
+    isLoading.value = true;
+    try {
+      await repository.deletar(id);
+      stateList.value = await repository.buscarTodos();
+    } catch (e) {
+      error.value = 'Error ao deletar Campanha';
+    } finally {
+      isLoading.value = false;
+    }
+  }
+}
